167e727e30941261610cc3d148699bb68114c7ca
jinhee kim <wlsgml0229@naver.com>
2023-01-03 01:17:19 +0900

    라우터 추가

--- sleack-front/layouts/App.tsx
+++ sleack-front/layouts/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
+import Login from "@pages/Login";
+import SignUp from "@pages/SignUp";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  return <div>초기 세팅입니다.</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
