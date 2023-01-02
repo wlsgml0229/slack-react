7cf1fda3ebe602b74241107e2e24a82a183e25a8
jinhee kim <wlsgml0229@naver.com>
2023-01-03 01:24:09 +0900

    코드 스플리팅 추가

--- sleack-front/layouts/App.tsx
+++ sleack-front/layouts/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router";
-import Login from "@pages/Login";
-import SignUp from "@pages/SignUp";
+
 import { BrowserRouter } from "react-router-dom";
+import loadble from "@loadable/component";
+
+//코드 스플리팅
+const Login = loadble(() => import("@pages/Login"));
+const SignUp = loadble(() => import("@pages/SignUp"));
 
 const App = () => {
   return (
