7cc334d67e1d000612ab095781a3ffad6c66218f
wlsgml0229 <wlsgml0229@naver.com>
2023-01-08 17:41:52 +0900

    커스텀 훅 생성

--- sleack-front/pages/SignUp/index.tsx
+++ sleack-front/pages/SignUp/index.tsx
@@ -1,21 +1,14 @@
+import useInput from "@hooks/useInput";
 import React, { useCallback, useState } from "react";
 import { Form, Error, Label, Input, LinkContainer, Button, Header } from "./style";
 
 //커스텀 컴포넌트 추가
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordCheck, setPasswordCheck] = useState('');
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, , setPassword] = useInput('');
+  const [passwordCheck, , setPasswordCheck] = useInput('');
   const [mismatchError, setMissMatchError] = useState(false);
-
-  const onChangeEmail = useCallback((e) => {
-    setEmail(e.target.value)
-  },[]);
-
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value)
-  },[]);
 
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value)
