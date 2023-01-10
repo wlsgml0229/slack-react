5c8c1b8505b72b3da7d9ff6300de0a0bbe1bb677
jinhee kim <wlsgml0229@naver.com>
2023-01-10 23:42:36 +0900

    swr 설치 및 로그인 컴포넌트 작업

--- sleack-front/pages/Login/index.tsx
+++ sleack-front/pages/Login/index.tsx
@@ -1,6 +1,64 @@
-import React from "react";
+import useInput from "@hooks/useInput";
+import {
+  Success,
+  Form,
+  Error,
+  Label,
+  Input,
+  LinkContainer,
+  Button,
+  Header,
+} from "@pages/SignUp/style";
+// import fetcher from '@utils/fetcher';
+import axios from "axios";
+import React, { useCallback, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import useSWR from "swr";
 
-const Login = () => {
-  return <div>로그인</div>;
+const LogIn = () => {
+  const [logInError, setLogInError] = useState(false);
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+            />
+          </div>
+          {logInError && (
+            <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>
+          )}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
-export default Login;
+
+export default LogIn;
